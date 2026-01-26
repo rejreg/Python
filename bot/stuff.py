622c81f384da122bedfdf87a9e983c462cd7d7f7
rejreg <154370356+rejreg@users.noreply.github.com>
2026-01-27 01:24:25 +0530

    Update stuff.py

--- bot/stuff.py
+++ bot/stuff.py
@@ -1,20 +1,7 @@
-#    This file is part of the CompressorQueue distribution.
-#    Copyright (c) 2021
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, version 3.
-#
-#    This program is distributed in the hope that it will be useful, but
-#    WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-#    General Public License for more details.
-#
-# License can be found in <
-# https://github.com/> .
-
 from .worker import *
+from datetime import datetime
 
+START_TIME = datetime.now()
 
 async def up(event):
     if not event.is_private:
@@ -23,44 +10,48 @@ async def up(event):
     ed = dt.now()
     v = ts(int((ed - uptime).seconds) * 1000)
     ms = (ed - stt).microseconds / 1000
-    p = f"🌋Pɪɴɢ = {ms}ms"
+    p = f"Ping = {ms}ms"
     await event.reply(v + "\n" + p)
 
 
 async def start(event):
     await event.reply(
-        f"Hi `{event.sender.first_name}`\nDo I Need To Say Something?",
+        f"**Send me the video which you want to compress.**\n**Uptime: {str(datetime.now() - START_TIME).split('.')[0]}**",
         buttons=[
-            [Button.inline("HELP", data="ihelp")],
-            [
-                Button.url("Modified", url="https://t.me/tomanguy"),
-                Button.url("DEVELOPER", url="t.me/tomanguy"),
-            ],
+            [Button.inline("HELP", data="help")],
         ],
+    )
+
+async def zylern(event):
+    await event.reply(
+        f"""
+**Available Commands 🤖**
+
+/start - __Check Bot is Working Or Not__
+/help - __Get Detailed Help__
+/setcode - __Set Custom FFMPEG Code__
+/getcode - __Print Current FFMPEG Code__
+/logs - __Get Bot Logs__
+/ping - __Check Ping__
+/sysinfo - __Get System Info__
+/leech - __Leech Links And Compress Video__
+/renew - __Clear Cached Downloads__
+/clear - __Clear Queued Files__
+/showthumb - __Show Current Thumbnail__
+/speed - __Do A SpeedTest__
+/eval - __Execute An Argument__
+/bash - __Run Bash Commands__
+/cmds - __List Available Commands__
+"""
     )
 
 
 async def help(event):
-    await event.reply(
-        "This Bot Is Not Public**\n\n✔️Only Authenticated Users Will Use This Bot"
-    )
-
-
-async def ihelp(event):
     await event.edit(
-        "This Bot Is Not Public**\n\n✔️Only Authenticated Users Will Use This Bot",
-        buttons=[Button.inline("BACK", data="beck")],
+        f"""**To check current ffmpeg code you can use** /getcode\n\n**You can change your ffmpeg code by executing following command.**\n\n`/setcode -preset faster -c:v libx265 -s 1280x720 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1`\n\n**To set custom thumbnail send me the image.**\n\n**Do /cmds For More**"""
     )
-
-
-async def beck(event):
-    await event.edit(
-        f"Hi `{event.sender.first_name}`\nThis Bot Is Not Public**\n\n✔️Only Authenticated Users Will Use This Bot",
-        buttons=[
-            [Button.inline("HELP", data="ihelp")],
-            [
-                Button.url("Modified", url="https://t.me/tomanguy"),
-                Button.url("DEVELOPER", url="t.me/tomanguy"),
-            ],
-        ],
+    
+async def ihelp(e):
+    await e.reply(
+        f"""**To check current ffmpeg code you can use** /getcode\n\n**You can change your ffmpeg code by executing following command.**\n\n`/setcode -preset faster -c:v libx265 -s 1280x720 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1`\n\n**To set custom thumbnail send me the image.**\n\n**Do /cmds For More**"""
     )
